d01150f7010b0a5e1d8f9ef7c134d55bd7852797
Tatyana Bogatyreva <Tatyana.Bogatyreva@FLEXDATABASES.COM>
2022-02-09 20:57:27 +0300

    Add project files.

---
+++ addressbook_test_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_test_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_test_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("979e0d0f-e59b-443a-8516-8df39c6ab93e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_test_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace addressbook_test_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            {
+                List<GroupData> oldGroups = app.Groups.GetGroupList();
+                GroupData GroupToDelete = new GroupData()
+                {
+                    Id = "0"
+                };
+                app.Groups.Remove(GroupToDelete);
+                List<GroupData> newGroups = app.Groups.GetGroupList();
+                oldGroups.Remove(oldGroups[0]);
+                oldGroups.Sort();
+                newGroups.Sort();
+                Assert.AreEqual(oldGroups, newGroups);
+            }
+        }
+    }
+}
+
+
